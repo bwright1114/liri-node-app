eb37afe8b0988b1c46fb1b3c12696f33dd3779ce
Brittni Wright <bwright1114@icloud.com>
2017-04-23 19:35:55 -0400

    the first commit,i.e. the rought draft

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '95R7xL8g1kXXdIkVMYkPep0OE',
+  consumer_secret: 'Z2IGdcDUFlB8SPO7fovxUc3ngMCZkUUEzzk7UWY71Qr8Ve2HVo',
+  access_token_key: '855218778858278912-H25OHs9pt6VV5ZaoJZ89hfjsG9fTIt7',
+  access_token_secret: 'bkpFilXwr3166xVu9Le0IJXNQt9OzKwAGOcoRlAh1lhua',
+}
